2029cdccc069b39abb6a3bdcb674fd92b9e5f3c3
root <root@kolya.localdomain>
2024-11-25 15:51:42 +0300

    myapp->models.py

---
+++ myapp/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+# Модель для таблицы area_of_expertise
+class AreaOfExpertise(models.Model):
+    area = models.CharField(max_length=100)  # Название области специализации
+
+    def __str__(self):
+        return self.area
+
+
+# Модель для таблицы author
+class Author(models.Model):
+    surname = models.CharField(max_length=50)  # Фамилия автора
+    name = models.CharField(max_length=50)  # Имя автора
+    patronymic = models.CharField(max_length=50, blank=True, null=True)  # Отчество автора (необязательно)
+
+    def __str__(self):
+        return f"{self.surname} {self.name} {self.patronymic or ''}".strip()
+
+
+# Модель для таблицы books
+class Book(models.Model):
+    name = models.CharField(max_length=100)  # Название книги
+    number_of_pages = models.IntegerField(blank=True, null=True)  # Количество страниц
+    year_of_publication = models.IntegerField(blank=True, null=True)  # Год публикации
+    area_of_expertise = models.ForeignKey(
+        AreaOfExpertise,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="books"
+    )  # Связь с таблицей AreaOfExpertise
+    authors = models.ManyToManyField(Author, related_name="books")  # Many-to-Many связь с Author
+
+    def __str__(self):
+        return self.name
