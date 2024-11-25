a423c303410fbc6bb46c6b9f214195165eb82bcc
LiHiy93 <150333101+LiHiy93@users.noreply.github.com>
2024-11-25 18:03:40 +0300

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Book, Author, AreaOfExpertise
+from .serializers import (
+    BookSerializer, BookCreateUpdateSerializer, 
+    AuthorSerializer, AreaOfExpertiseSerializer
+)
+
+# Books
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookCreateUpdateSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookCreateUpdateSerializer
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+
+# Authors
+class AuthorListView(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class AuthorCreateUpdateDeleteView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+# Areas of Expertise
+class AreaOfExpertiseListView(generics.ListAPIView):
+    queryset = AreaOfExpertise.objects.all()
+    serializer_class = AreaOfExpertiseSerializer
+
+class AreaOfExpertiseCreateUpdateDeleteView(generics.ListCreateAPIView):
+    queryset = AreaOfExpertise.objects.all()
+    serializer_class = AreaOfExpertiseSerializer
